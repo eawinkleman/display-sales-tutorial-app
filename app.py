960c8e3c4006c123b4d111ffbb1eff8b24d4a556
Eric Winkleman <157304057+eawinkleman@users.noreply.github.com>
2025-02-20 21:59:50 +0000

    pylint changes and requirements updated

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+"""Module providing an app that shows daily vs monthly sales, by product."""
+
+import time
+import string
+from datetime import date, timedelta
 import streamlit as st
 import pandas as pd
 import numpy as np
-from datetime import date, timedelta
-import string
-import time
 
 
 @st.cache_data
@@ -15,18 +17,20 @@ def get_data():
     products = dict(zip(product_names, average_daily_sales))
 
     data = pd.DataFrame({})
-    sales_dates = np.arange(date(2023, 1, 1), date(2024, 1, 1), timedelta(days=1))
+    sales_dates = np.arange(date(2023, 1, 1), date(
+        2024, 1, 1), timedelta(days=1))
     for product, sales in products.items():
         data[product] = np.random.normal(sales, 300, len(sales_dates)).round(2)
     data.index = sales_dates
     data.index = data.index.date
-    
     return data
+
 
 @st.fragment
 def show_daily_sales(data):
+    """Shows the daily sales for Widget A through Widget Z in 2023"""
     time.sleep(1)
-    with st.container(height=100): # Container
+    with st.container(height=100):  # Container
         selected_date = st.date_input(
             "Pick a day ",
             value=date(2023, 1, 1),
@@ -45,40 +49,45 @@ def show_daily_sales(data):
     if is_new_month:
         st.rerun()
 
-    with st.container(height=550): # Container
+    with st.container(height=550):  # Container
         st.header(f"Best sellers, {selected_date:%m/%d/%y}")
         top_ten = data.loc[selected_date].sort_values(ascending=False)[0:10]
-        cols = st.columns([1,4])
+        cols = st.columns([1, 4])
         cols[0].dataframe(top_ten)
         cols[1].bar_chart(top_ten)
 
-    with st.container(height=550): # Container
+    with st.container(height=550):  # Container
         st.header(f"Worst sellers, {selected_date:%m/%d/%y}")
         bottom_ten = data.loc[selected_date].sort_values()[0:10]
-        cols = st.columns([1,4])
+        cols = st.columns([1, 4])
         cols[0].dataframe(bottom_ten)
         cols[1].bar_chart(bottom_ten)
 
+
 def show_monthly_sales(data):
+    """Shows the Monthly sales for Widget A through Widget Z in 2023"""
     time.sleep(1)
     selected_date = st.session_state.selected_date
     this_month = selected_date.replace(day=1)
-    next_month = (selected_date.replace(day=28) + timedelta(days=4)).replace(day=1)
+    next_month = (selected_date.replace(day=28) +
+                  timedelta(days=4)).replace(day=1)
 
-    st.container(height=100, border=False) # Container
+    st.container(height=100, border=False)  # Container
 
-    with st.container(height=550): # Container
+    with st.container(height=550):  # Container
         st.header(f"Daily sales for all products, {this_month:%B %Y}")
-        monthly_sales = data[(data.index < next_month) & (data.index >= this_month)]
+        monthly_sales = data[(data.index < next_month) &
+                             (data.index >= this_month)]
         st.write(monthly_sales)
 
-    with st.container(height=550): # Container
+    with st.container(height=550):  # Container
         st.header(f"Total sales for all products, {this_month:%B %Y}")
         st.bar_chart(monthly_sales.sum())
 
+
 st.set_page_config(layout="wide")
 
-data = get_data()
+d = get_data()
 
 st.title("Daily vs Monthly Sales, By Product")
 st.markdown("This app shows the 2023 daily sales for Widgit A through Widgit Z.")
@@ -86,6 +95,6 @@ st.markdown("This app shows the 2023 daily sales for Widgit A through Widgit Z."
 daily, monthly = st.columns(2)
 
 with daily:
-    show_daily_sales(data)
-with monthly:   
-    show_monthly_sales(data)
+    show_daily_sales(d)
+with monthly:
+    show_monthly_sales(d)
